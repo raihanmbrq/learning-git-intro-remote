fe5d07f4ca1c8cd89c63c8f49ac7ddd6db326a18
raihanmbrq <mubaroqraihan@gmail.com>
2026-06-13 16:57:19 +0700

    Pelajaran Pertama

---
+++ Latihan1.java
@@ -0,0 +1,23 @@
+public class Latihan1 {
+    public static void main(String[] args) {
+        // 1. Menyimpan data produk (Tipe Data String)
+        String productName = "Apple iPhone 15 Pro";
+
+        // 2. Menyimpan harga produk (Tipe Data double karena ada desimal)
+        double productPrice = 999.99;
+
+        // 3. Menyimpan jumlah item yang dibeli (Tipe Data int)
+        int quantity = 2;
+
+        // 4. Menyimpan status kupon diskon (Tipe Data boolean)
+        boolean isCouponApplied = true;
+
+        // Melakukan kalkulasi total harga menggunakan Arithmetic Operator (*)
+        double totalCost = productPrice * quantity;
+
+        // Menampilkan data ke console (Log output)
+        System.out.println("Nama Produk: " + productName);
+        System.out.println("Total Pembayaran: $" + totalCost);
+        System.out.println("Apakah Diskon Aktif? " + isCouponApplied);
+    }
+}
---
+++ RegistrasiBank.java
@@ -0,0 +1,26 @@
+public class RegistrasiBank {
+    public static void main(String[] args) {
+        // 1. Deklarasikan variabel untuk Nama Lengkap (John Doe)
+        String NamaLengkap = "John Doe";
+
+        // 2. Deklarasikan variabel untuk Umur (27)
+        int umur = 27;
+
+        // 3. Deklarasikan variabel untuk Saldo Awal Deposit (500000.75)
+        double SaldoAwal = 500000.75;
+
+        // 4. Deklarasikan variabel untuk Status Nasabah Baru (true)
+        boolean NasabahBaru = true;
+
+        // 5. Buat variabel boolean 'isEligibleForBonus'
+        // yang membandingkan: Apakah umur lebih besar dari 25?
+        boolean isEligibleForBonus = umur > 25;
+
+        // 6. Cetak semua variabel ke terminal menggunakan System.out.println()
+        System.out.println("Nama Lengkap: " + NamaLengkap);
+        System.out.println("Umur : " + umur);
+        System.out.println("Saldo Awal : " + SaldoAwal);
+        System.out.println("Apakah " + NamaLengkap + "adalah nasabah baru? " + NasabahBaru);
+        System.out.println("Apakah menerima bonus? " + isEligibleForBonus);
+    }
+}
